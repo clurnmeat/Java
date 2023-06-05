0cae8e1c3ed6505552c0b4a170e6f78b5b842ba4
macbook <walkerdionte@outlook.com>
2023-06-05 13:17:33 -0500

    Started cupcake

---
+++ src/Cupcake.java
@@ -0,0 +1,19 @@
+class Cupcake{
+    public double price;
+
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+
+    }
+
+    public void type(){
+        System.out.println("A basic, generic cupcake, with vanilla frosting");
+    }
+
+
+}
---
+++ src/RedVelvet.java
@@ -0,0 +1,10 @@
+public class RedVelvet extends Cupcake {
+    public RedVelvet(){
+
+
+    }
+    public void type(){
+        System.out.println("A red velvet based cupcake, with cream cheese frosting.");
+
+    }
+}
