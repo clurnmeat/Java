ee4e7d680a3f1dc3db36bd1e3de5741cc98a1516
macbook <walkerdionte@outlook.com>
2023-06-18 19:13:59 -0500

    Started part 2 of project 3

---
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -0,0 +1,13 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MovieController {
+
+    @RequestMapping("/")
+     public String getIndexPage(){
+        return "index";
+    }
+}
