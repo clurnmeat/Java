e14cd3e3158e6ef577326275a858b9cc2dae2c4d
macbook <walkerdionte@outlook.com>
2023-06-27 06:42:30 -0500

    Started project 2 of unit 4

---
+++ noteApp/src/main/java/com/devmountain/noteApp/entities/User.java
@@ -0,0 +1,39 @@
+package com.devmountain.noteApp.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    @Column
+    private String password;
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+}
