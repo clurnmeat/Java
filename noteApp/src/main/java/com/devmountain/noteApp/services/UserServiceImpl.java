818ae1ec1e53cfa89e8a241eaa7ee1bb04c84b94
macbook <walkerdionte@outlook.com>
2023-06-28 09:36:24 -0500

    Started project 4 of unit 4

--- noteApp/src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
+++ noteApp/src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
         List<String> response = new ArrayList<>();
         User user = new User(userDto);
         userRepository.saveAndFlush(user);
-        response.add("User Added Successfully");
+        response.add("http://localhost:8080/login.html");
         return response;
     }
 
@@ -36,10 +36,10 @@ public class UserServiceImpl implements UserService {
         Optional<User> userOptional = userRepository.findByUsername(userDto.getUsername());
         if (userOptional.isPresent()) {
             if (passwordEncoder.matches(userDto.getPassword(), userOptional.get().getPassword())) {
-                response.add("User Login Successful");
+                response.add("http://localhost:8080/home.html");
                 response.add(String.valueOf(userOptional.get().getId()));
             } else {
-                response.add("Username or Password is incorrect!");
+                response.add("http://localhost:8080/register.html");
             }
 
         }
